affb0d73b7e5cc39b31b9500ce96cd4170bd17ea
Lil Foo <robertelvisodoch@gmail.com>
2017-03-04 23:57:02 +0300

    Remove li tags

--- application/views/friend-requests.php
+++ application/views/friend-requests.php
@@ -11,25 +11,23 @@ require_once('common/user-page-start.php');
         <p><span class="glyphicon glyphicon-info-sign"></span> No friend requests to show.</p>
     </div>
     <?php else: ?>
-    <ul class="friends">
+    <div class="friends">
         <?php foreach($friend_requests as $fr): ?>
-        <li>
-            <div class="media">
-                <div class="media-left">
-                    <img class="media-object" src="<?= $fr['profile_pic_path'] ?>"
-                    alt="<?= $fr['name']; ?>">
-                </div>
-                <div class="media-body">
-                    <h4 class="media-heading">
-                        <a href="<?= base_url("user/index/{$fr['user_id']}"); ?>"><?= $fr['name']; ?></a>
-                    </h4>
-                    <a href="<?= base_url("user/accept-friend/{$fr['user_id']}"); ?>"
-                        class="btn btn-sm">Confirm</a>
-                </div>
+        <div class="media">
+            <div class="media-left">
+                <img class="media-object" src="<?= $fr['profile_pic_path'] ?>"
+                alt="<?= $fr['name']; ?>">
             </div>
-        </li>
+            <div class="media-body">
+                <h4 class="media-heading">
+                    <a href="<?= base_url("user/index/{$fr['user_id']}"); ?>"><?= $fr['name']; ?></a>
+                </h4>
+                <a href="<?= base_url("user/accept-friend/{$fr['user_id']}"); ?>"
+                    class="btn btn-xs">Confirm</a>
+            </div>
+        </div>
         <?php endforeach; ?>
-    </ul>
+    </div>
     <?php endif; ?>
 </div><!-- .box -->
 <?php if ($has_next): ?>
