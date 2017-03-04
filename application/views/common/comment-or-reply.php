77dab7ba0bc65e63173c6405530b2a614c803855
Lil Foo <robertelvisodoch@gmail.com>
2017-03-04 21:54:05 +0300

    show comment or reply using bootstrap's media object

--- application/views/common/comment-or-reply.php
+++ application/views/common/comment-or-reply.php
@@ -1,14 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 ?>
 
 <div class="box">
-    <article id="<?= $object; ?>">
-        <header>
-            <a href="<?= base_url("user/index/{$$object['commenter_id']}"); ?>"><?= $$object['commenter']; ?></a>
-        </header>
-        <p><?= htmlspecialchars($$object['comment']); ?></p>
-        <footer>
+    <div id="<?= $object; ?>" class="media">
+        <div class="media-left">
+            <img class="media-object" src="<?= $$object['profile_pic_path']; ?>"
+                alt="<?= $$object['commenter']; ?>'s photo">
+        </div>
+        <div class="media-body">
+            <h4 class="media-heading">
+                <a href="<?= base_url("user/index/{$$object['commenter_id']}"); ?>">
+                    <?= $$object['commenter']; ?>
+                </a>
+            </h4>
+            <p class="comment"><?= htmlspecialchars($$object['comment']); ?></p>
+
             <small class="time"><span class="glyphicon glyphicon-time"></span> <?= $$object['timespan']; ?> ago</small>
             <?php
             if ($$object['num_likes'] > 0) {
@@ -22,21 +30,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     print ($comment['num_replies'] == 1) ? " reply" : " replies";
                     print "</a>";
                 }
+            }
             ?>
-                <form action="<?= base_url("comment/reply/{$comment['comment_id']}"); ?>" method="post" accept-charset="utf-8" role="form">
-                    <input type="text" name="reply" placeholder="Write a reply..." class="fluid
-                    <?php
-                    if (isset($reply_error)) {
-                        print " has-error";
-                    }
-                    ?>">
-                    <?php
-                    if (isset($reply_error)) {
-                        print "<span class='error'>{$reply_error}</span>";
-                    }
-                    ?>
-                </form>
-            <?php } ?>
-        </footer>
-    </article>
+        </div>
+    </div>
+    <?php if ($object == 'comment') { ?>
+        <form action="<?= base_url("comment/reply/{$comment['comment_id']}"); ?>" method="post" accept-charset="utf-8" role="form">
+            <input type="text" name="reply" placeholder="Write a reply..." class="fluid
+            <?php
+            if (isset($reply_error)) {
+                print " has-error";
+            }
+            ?>">
+            <?php
+            if (isset($reply_error)) {
+                print "<span class='error'>{$reply_error}</span>";
+            }
+            ?>
+        </form>
+    <?php } ?>
 </div><!-- box -->
