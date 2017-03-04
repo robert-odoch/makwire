30086dc2df7bb372290471cc24742a48b0d5d240
Lil Foo <robertelvisodoch@gmail.com>
2017-03-04 22:56:23 +0300

    Remove li tags

--- application/views/common/active-users.php
+++ application/views/common/active-users.php
@@ -3,23 +3,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 if (count($chat_users) > 0):
 ?>
-    <ul class="active-users">
+    <div class="active-users">
         <?php foreach($chat_users as $chat_user): ?>
-        <li>
-            <div class="media">
-                <div class="media-left media-middle">
-                    <img class="media-object" src="<?= $chat_user['profile_pic_path']; ?>"
-                    alt="<?= $chat_user['profile_name']?>">
-                </div>
-                <div class="media-body">
-                    <h4 class="media-heading">
-                        <a href="<?= base_url("user/send-message/{$chat_user['friend_id']}"); ?>">
-                            <?= $chat_user['profile_name']; ?>
-                        </a>
-                    </h4>
-                </div>
+        <div class="media">
+            <div class="media-left media-middle">
+                <img class="media-object" src="<?= $chat_user['profile_pic_path']; ?>"
+                alt="<?= $chat_user['profile_name']?>">
             </div>
-        </li>
+            <div class="media-body">
+                <h4 class="media-heading">
+                    <a href="<?= base_url("user/send-message/{$chat_user['friend_id']}"); ?>">
+                        <?= $chat_user['profile_name']; ?>
+                    </a>
+                </h4>
+            </div>
+        </div>
         <?php endforeach; ?>
-    </ul>
+    </div>
 <?php endif; ?>
