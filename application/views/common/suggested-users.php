e09c53c770da3bb3d39ee034e984551b4c0893f4
Lil Foo <robertelvisodoch@gmail.com>
2017-03-04 13:10:14 +0300

    Link a suggested user's name to his timeline.
    Initially there was a restriction that a user should not view the timeline of
    another user if they are not friends, however that restriction has now been
    remove but still the cannot comment, like or share the posts of this user's.

--- application/views/common/suggested-users.php
+++ application/views/common/suggested-users.php
@@ -14,7 +14,7 @@ if (count($people_you_may_know) > 0) {
                 </div>
                 <div class="media-body">
                     <h4 class="media-heading">
-                        <a href="<?= base_url("user/profile/{$p['user_id']}"); ?>"><?= $p['profile_name']; ?></a>
+                        <a href="<?= base_url("user/index/{$p['user_id']}"); ?>"><?= $p['profile_name']; ?></a>
                     </h4>
                     <a href="<?= base_url("user/add-friend/{$p['user_id']}"); ?>"
                         class="btn btn-sm">Add friend</a>
