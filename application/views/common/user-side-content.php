2105c2b6d36f7ed9f19a51f217f4ff49e848b8f7
Lil Foo <robertelvisodoch@gmail.com>
2017-03-04 10:31:30 +0300

    Change the user-side-content div to also use media object.
    Apply media object to a user's profile picture and name.

--- application/views/common/user-side-content.php
+++ application/views/common/user-side-content.php
@@ -3,12 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div class="side-content">
     <aside>
-        <div id="primary-user">
-            <a href="<?= base_url("upload/profile-picture"); ?>" title="Change Profile Picture">
-            <img src="<?= $profile_pic_path; ?>" alt="<?= $primary_user; ?>'s photo" class="profile-pic">
-            </a>
-            <a href="<?= base_url("user/index/{$_SESSION['user_id']}"); ?>"><?= $primary_user; ?></a>
+        <div id="primary-user" class="media">
+            <div class="media-left media-middle">
+                <a href="<?= base_url("upload/profile-picture"); ?>" title="Change profile picture">
+                <img src="<?= $profile_pic_path; ?>" alt="<?= $primary_user; ?>'s photo"
+                    class="media-object">
+                </a>
+            </div>
+            <div class="media-body">
+                <h4 class="media-heading">
+                    <a href="<?= base_url("user/index/{$_SESSION['user_id']}"); ?>">
+                        <?= $primary_user; ?>
+                    </a>
+                </h4>
+            </div>
         </div>
+
         <nav role="navigation" class="user-nav">
             <ul>
                 <li><a href="<?= base_url("user/profile"); ?>">Edit Profile</a></li>
