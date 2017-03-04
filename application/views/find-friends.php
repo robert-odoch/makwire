6427f7ce0507fc15ef0ff7026584d8254e25ec56
Lil Foo <robertelvisodoch@gmail.com>
2017-03-04 23:25:13 +0300

    Remove li tags

--- application/views/find-friends.php
+++ application/views/find-friends.php
@@ -19,25 +19,23 @@ require_once('common/user-page-start.php');
 <?php if (count($suggested_users) > 0): ?>
 <div class="box">
     <h4>People you may know</h4>
-    <ul class="suggested-users">
+    <div class="suggested-users">
         <?php foreach ($suggested_users as $user): ?>
-        <li>
-            <div class="media">
-                <div class="media-left">
-                    <img class="media-object" src="<?= $user['profile_pic_path']; ?>"
-                    alt="<?= $user['profile_name']; ?>">
-                </div>
-                <div class="media-body">
-                    <h4 class="media-heading">
-                        <a href="<?= base_url("user/index/{$user['user_id']}"); ?>"><?= $user['profile_name']; ?></a>
-                    </h4>
-                    <a href="<?= base_url("user/add-friend/{$user['user_id']}"); ?>"
-                        class="btn btn-sm">Add friend</a>
-                </div>
+        <div class="media">
+            <div class="media-left">
+                <img class="media-object" src="<?= $user['profile_pic_path']; ?>"
+                alt="<?= $user['profile_name']; ?>">
             </div>
-        </li>
+            <div class="media-body">
+                <h4 class="media-heading">
+                    <a href="<?= base_url("user/index/{$user['user_id']}"); ?>"><?= $user['profile_name']; ?></a>
+                </h4>
+                <a href="<?= base_url("user/add-friend/{$user['user_id']}"); ?>"
+                    class="btn btn-sm">Add friend</a>
+            </div>
+        </div>
         <?php endforeach; ?>
-    </ul>
+    </div>
 </div><!-- .box -->
 <?php if ($has_next): ?>
 <div class="box more">
