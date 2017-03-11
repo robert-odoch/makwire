38f1187e14bde400a1033994e933fc8b12a1b61c
Lil Foo <robertelvisodoch@gmail.com>
2017-03-11 19:45:56 +0300

    Change error to message

--- application/views/show-permission-denied.php
+++ application/views/show-permission-denied.php
@@ -6,6 +6,6 @@ require_once('common/user-page-start.php');
 
 <div class="box">
     <div class="alert alert-danger">
-        <p><span class="glyphicon glyphicon-alert"></span> <?= $error; ?></p>
+        <p><span class="glyphicon glyphicon-alert"></span> <?= $message; ?></p>
     </div>
 </div>
