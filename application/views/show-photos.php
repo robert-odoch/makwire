80557ac78c6184ef87db2a6e134cefe006f8ef9a
Lil Foo <robertelvisodoch@gmail.com>
2017-03-18 19:58:28 +0300

    Add view for showing user photos

---
+++ application/views/show-photos.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once('common/user-page-start.php');
+
+if ($is_visitor) {
+    define('PAGE', 'photos');
+    require_once("common/secondary-user-nav.php");
+}
+?>
+<div class="box">
+    <?php if (!$is_visitor) { ?>
+    <h4>Photos</h4>
+    <?php } ?>
+    
+    <?php if (count($photos) == 0) { ?>
+        <div class="alert alert-info">
+            <span class="glyphicon glyphicon-info-sign"></span> No photos to show.
+        </div>
+    <?php } else { ?>
+
+    <div class="container-fluid">
+        <div class="row gallery">
+            <?php foreach ($photos as $photo) { ?>
+            <div class="col-xs-6 col-md-3">
+                <a href="<?= base_url("user/photo/{$photo['photo_id']}"); ?>" title="view photo">
+                    <img src="<?= $photo['web_path']; ?>" alt="<?= $photo['alt']; ?>"
+                        class="img-responsive">
+                </a>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
+    <?php } ?>
+</div><!-- box -->
+<?php if ($has_next) { ?>
+    <div class="box more">
+        <a href="<?= base_url("user/photos/{$user_id}/{$next_offset}"); ?>">View more photos</a>
+    </div>
+<?php } ?>
