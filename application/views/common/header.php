f900bb1510c0a0524081cc10ca641dd7ae02e256
Lil Foo <robertelvisodoch@gmail.com>
2017-03-19 21:26:14 +0300

    Add lang attribute to html

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
