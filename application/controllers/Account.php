434476a03094d9674609993e66ae65ef1d9d72dd
Lil Foo <robertelvisodoch@gmail.com>
2017-03-25 11:50:16 +0300

    Prelimary version of the Account controller

---
+++ application/controllers/Account.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function forgot_password()
+    {
+
+    }
+
+    public function change_passord()
+    {
+
+    }
+
+    public function deactivate()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+}
+?>
