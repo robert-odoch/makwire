14fbdd15be7f450bea8ab515d7d827c0d006d2ce
Lil Foo <robertelvisodoch@gmail.com>
2017-03-25 11:49:37 +0300

    Add a Settings controller

---
+++ application/controllers/Settings.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Settings extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        session_start();
+        if (!isset($_SESSION['user_id']) || empty($_SESSION['user_id'])) {
+            redirect(base_url('login'));
+        }
+
+        $this->load->model(['user_model']);
+
+        // Check whether the user hasn't been logged out from some where else.
+        $this->user_model->confirm_logged_in();
+    }
+
+    public function account()
+    {
+        $data = $this->user_model->initialize_user();
+        $data['title'] = 'Account settings';
+        $this->load->view('common/header', $data);
+
+        $this->load->view('account-settings');
+        $this->load->view('common/footer');
+    }
+
+    public function emails()
+    {
+        $data = $this->user_model->initialize_user();
+        $data['title'] = 'Email settings';
+        $this->load->view('common/header', $data);
+
+        $this->load->view('email-settings');
+        $this->load->view('common/footer');
+
+    }
+
+    public function notifications()
+    {
+        $data = $this->user_model->initialize_user();
+        $data['title'] = 'Notification settings';
+        $this->load->view('common/header', $data);
+
+        $this->load->view('notification-settings');
+        $this->load->view('common/footer');
+    }
+
+    public function blocked_users()
+    {
+        $data = $this->user_model->initialize_user();
+        $data['title'] = 'Blocked users';
+        $this->load->view('common/header', $data);
+
+        $this->load->view('block-user');
+        $this->load->view('common/footer');
+    }
+
+}
+?>
