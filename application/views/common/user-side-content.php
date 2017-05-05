6409e1a16375c999bfe4ac54755887e549b58842
Lil Foo <robertelvisodoch@gmail.com>
2017-05-05 10:28:42 +0300

    Fix broken link

--- application/views/common/user-side-content.php
+++ application/views/common/user-side-content.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <nav role="navigation" class="user-nav">
             <ul>
                 <li><a href="<?= base_url('user/profile'); ?>">Profile</a></li>
-                <li><a href="<?= base_url('user/settings/account'); ?>">Settings</a></li>
+                <li><a href="<?= base_url('settings/account'); ?>">Settings</a></li>
             </ul>
         </nav>
     </aside>
