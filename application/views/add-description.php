3a059f167d657b3198a90f24df4e4d1fdbd041b8
Lil Foo <robertelvisodoch@gmail.com>
2017-05-05 18:14:20 +0300

    Use textarea instead of text input for adding photo description

--- application/views/add-description.php
+++ application/views/add-description.php
@@ -5,19 +5,12 @@ require_once('common/user-page-start.php');
 ?>
 
 <div class="box">
+    <h4>Say something about this photo</h4>
     <form action="<?= base_url("photo/add-description/{$photo['photo_id']}"); ?>"
         id="add-description" method="post" accept-charset="utf-8" role="form">
-        <fieldset>
-            <div class="form-group">
-                <label for="description">
-                    Say something about this photo.
-                </label>
-                <img src="<?= $photo['web_path']; ?>" alt="">
-                <input type="text" name="description" placeholder="comment..."
-                    id="description" class="fluid">
-            </div>
-        </fieldset>
 
+        <textarea name="description" placeholder="Your say..."id="description" class="fluid"></textarea>
+        <img src="<?= $photo['web_path']; ?>">
         <input type="submit" name="submit" value="Submit" class="btn btn-sm">
     </form>
 </div>
