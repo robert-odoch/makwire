2cadc6832fc698bd3bb32170cc0e3e9815b43d0a
Lil Foo <robertelvisodoch@gmail.com>
2017-05-20 20:03:59 +0300

    Implement the Replyable interface.

--- application/models/classes/SimpleComment.php
+++ application/models/classes/SimpleComment.php
@@ -3,8 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 require_once('Object.php');
 require_once(dirname(__FILE__) . '/../interfaces/Likeable.php');
+require_once(dirname(__FILE__) . '/../interfaces/Replyable.php');
 
-class SimpleComment extends Object implements Likeable
+class SimpleComment extends Object implements Likeable, Replyable
 {
     public function __construct($id, $type, $ownerId)
     {
