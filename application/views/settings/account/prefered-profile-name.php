34e7e54cf9c48cdd81d9f88aa7520800fbe651a6
Lil Foo <robertelvisodoch@gmail.com>
2017-05-21 22:32:31 +0300

    Implement code for setting prefered profile name

--- application/views/settings/account/prefered-profile-name.php
+++ application/views/settings/account/prefered-profile-name.php
@@ -11,22 +11,17 @@
             <div class="main-content">
                 <div class="box">
                     <h4>Prefered display name</h4>
-                    <form action="<?= base_url('account/set-prefered-name'); ?>" method="post" accept-charset="utf-8" role="form">
-                        <fieldset>
-                            <div class="radio">
-                                <label for="name1">
-                                    <input type="radio" name="name" value="Odoch Robert" id="name1">
-                                    Odoch Robert
-                                </label>
-                            </div>
-                            <div class="radio">
-                                <label for="name2">
-                                    <input type="radio" name="name" value="Robert Odoch" id="name2">
-                                    Robert Odoch
-                                </label>
-                            </div>
-                        </fieldset>
-
+                    <form action="<?= base_url('account/set-prefered-name'); ?>"
+                        method="post" accept-charset="utf-8" role="form">
+                        <?php foreach ($name_combinations as $n) { ?>
+                        <div class="radio">
+                            <label for="<?= implode('-', explode(' ', $n)); ?>">
+                                <input type="radio" name="prefered_name"
+                                    value="<?= $n; ?>" id="<?= implode('-', explode(' ', $n)); ?>">
+                                <?= $n; ?>
+                            </label>
+                        </div>
+                        <?php } ?>
                         <input type="submit" name="submit" value="Update" class="btn btn-sm">
                     </form>
                 </div>
