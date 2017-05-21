a20827e38eef0e2a9bb7b00a01439a87d86db1a9
Lil Foo <robertelvisodoch@gmail.com>
2017-05-21 17:16:47 +0300

    Change wording of brand name

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -33,7 +33,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="<?= base_url(); ?>">Mak Wire</a>
+                        <a class="navbar-brand" href="<?= base_url(); ?>">Makwire</a>
                     </div>
                     <div id="navbar" class="navbar-collapse collapse">
                         <ul class="nav navbar-nav navbar-right">
