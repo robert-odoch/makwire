3c9a01a75b2510b2981bead684474795b41df29a
Lil Foo <robertelvisodoch@gmail.com>
2017-06-01 19:47:03 +0300

    Remove photo from form

--- application/views/add-description.php
+++ application/views/add-description.php
@@ -3,16 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require_once('common/user-page-start.php');
 ?>
 
-<div class="box">
+<div id="add-description" class="box">
     <h4>Say something about this photo</h4>
+    <img src="<?= $photo['web_path']; ?>">
     <form action="<?= base_url("photo/add-description/{$photo['photo_id']}"); ?>"
-        id="add-description" method="post" accept-charset="utf-8" role="form">
+        method="post" accept-charset="utf-8" role="form">
         <textarea name="description" placeholder="Your say..."id="description" class="fluid
             <?php if (isset($error_message)) print ' has-error'; ?>"></textarea>
         <?php if (isset($error_message)) { ?>
             <span class='error'><?= $error_message; ?></span>
         <?php } ?>
-        <img src="<?= $photo['web_path']; ?>">
         <input type="submit" name="submit" value="Submit" class="btn btn-sm">
     </form>
 </div>
