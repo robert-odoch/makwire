4e781ac1f421af3d92a9e6f59438ab3b39ac57aa
Lil Foo <robertelvisodoch@gmail.com>
2017-06-04 19:55:29 +0300

    Change regex to carter for offset

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'login';
-$route['user/(:num)'] = 'user/index/$1';
+$route['user/(:num)(/:num)?'] = 'user/index/$1$2';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = TRUE;
