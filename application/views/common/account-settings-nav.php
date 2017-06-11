2b32023a2dc4983f46495e4d1bef5b6e54668791
Lil Foo <robertelvisodoch@gmail.com>
2017-06-11 11:41:06 +0300

    Provide link for a user to return to their home page.

--- application/views/common/account-settings-nav.php
+++ application/views/common/account-settings-nav.php
@@ -1,6 +1,24 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 <div class="side-content">
+    <div>
+        <div id="primary-user" class="media">
+            <div class="media-left media-middle">
+                <a href="<?= base_url("profile/change-profile-picture"); ?>" title="Change profile picture">
+                <img src="<?= $profile_pic_path; ?>" alt="<?= $primary_user; ?>"
+                    class="profile-pic">
+                </a>
+            </div>
+            <div class="media-body">
+                <h4 class="media-heading">
+                    <a href="<?= base_url("user/{$_SESSION['user_id']}"); ?>">
+                        <?= $primary_user; ?>
+                    </a>
+                </h4>
+            </div>
+        </div>
+    </div>
+
     <nav class="user-nav" role="navigation">
         <p><span class="glyphicon glyphicon-cog btn btn-sm"></span> <b>Settings</b></p>
         <ul>
