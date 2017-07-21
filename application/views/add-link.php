4ca605890d74b2cae3992aab419b20c9a0872dab
Lil Foo <robertelvisodoch@gmail.com>
2017-07-21 20:13:46 +0300

    Include status navigation bar

--- application/views/add-link.php
+++ application/views/add-link.php
@@ -4,18 +4,23 @@ require_once('common/user-page-start.php');
 ?>
 
 <div class='box'>
+
+    <?php
+    define('STATUS', 'link');
+    require_once(dirname(__FILE__) . '/common/status-nav.php');
+    ?>
+
     <div class='panel panel-default'>
-        <div class='panel-heading'>Add link to a resource (news, post, etc) from another website</div>
         <div class='panel-body'>
-            <?php if (isset($error)) { ?>
-            <div class='alert alert-danger' role='alert'>
-                <?= $error; ?>
-            </div>
-            <?php } ?>
             <form action='<?= base_url('video/new'); ?>' method='post' role='form'>
                 <div class='form-group'>
                     <label for='link-url'>URL for link:</label>
-                    <input type='url' name='link_url' id='link-url' class='fluid' required>
+                    <input type='url' name='link_url' id='link-url' class='fluid
+                            <?php if (isset($error_message)) print ' has-error'; ?>' required>
+                    <?php
+                    if (isset($error_message)) {
+                        print "<span class='error'>{$error_message}</span>";
+                    }?>
                 </div>
                 <input type='submit' name='submit' value='Submit' class='btn btn-sm'>
             </form>
