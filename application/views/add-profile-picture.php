23a56958754d1c66c5351c15564fa27a40b63808
Lil Foo <robertelvisodoch@gmail.com>
2017-07-21 20:29:53 +0300

    Put photo upload and profile pic change view code in separate files

---
+++ application/views/add-profile-picture.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once('common/user-page-start.php');
+?>
+
+<div class='box'>
+    <div class='panel panel-default'>
+        <div class='panel-heading'>Change profile picture</div>
+        <div class='panel-body'>
+            <?php if (isset($error)) { ?>
+            <div class='alert alert-danger' role='alert'>
+                <?= $error; ?>
+            </div>
+            <?php } ?>
+            <form action='<?= base_url('profile/change-profile-picture'); ?>' method='post'
+                enctype='multipart/form-data' role='form'>
+                <div class='form-group'>
+                    <label for='userfile'>Choose a photo:</label>
+                    <input type='file' name='userfile' id='userfile' required>
+                </div>
+                <input type='submit' name='submit' value='Upload' class='btn btn-sm'>
+            </form>
+        </div>
+    </div>
+</div>
