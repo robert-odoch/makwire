cecfbd07198ae9fd569581568e164951cf38280b
Lil Foo <robertelvisodoch@gmail.com>
2017-07-21 20:15:25 +0300

    Add link for adding status post

--- application/views/common/status-nav.php
+++ application/views/common/status-nav.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <ul class='nav nav-tabs'>
     <li role='presentation' <?php if (STATUS == 'post') print "class='active'"; ?>>
-        <a href='#'>
+        <a href='<?= base_url('news-feed'); ?>'>
             <span class='glyphicon glyphicon-edit' aria-hidden='true'></span> Status
         </a>
     </li>
