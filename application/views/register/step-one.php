858728facbd04735cca612e869cfac48868528fe
Lil Foo <robertelvisodoch@gmail.com>
2017-07-22 19:03:11 +0300

    Use single quotes for HTML attributes

--- application/views/register/step-one.php
+++ application/views/register/step-one.php
@@ -1,33 +1,33 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
-<div class="wrap-single">
-    <div role="main" class="main">
-        <div class="box">
+<div class='wrap-single'>
+    <div role='main' class='main'>
+        <div class='box'>
             <h4>Sign Up: step 1 of 3</h4>
             <?php if (isset($info_message)) { ?>
-                <div class="alert alert-info">
+                <div class='alert alert-info'>
                     <p><?= $info_message; ?></p>
                 </div>
             <?php } elseif (isset($success_message)) { ?>
-                <div class="alert alert-success">
+                <div class='alert alert-success'>
                     <p><?= $success_message; ?></p>
                 </div>
             <?php } else { ?>
-                <form action="<?= base_url('register/step-one'); ?>" method="post"
-                    accept-charset="utf-8" role="form">
+                <form action='<?= base_url('register/step-one'); ?>' method='post'
+                        accept-charset='utf-8' role='form'>
                     <fieldset>
-                        <div class="form-group">
-                            <label for="email">Email Address:</label>
-                            <span class="help-block">
+                        <div class='form-group'>
+                            <label for='email'>Email Address:</label>
+                            <span class='help-block'>
                                 Your Makerere University email address
                             </span>
-                            <input type="email" name="email" id="email" placeholder="e.g., name@college.mak.ac.ug"
-                                class="fluid
-                                <?php
-                                if (isset($error_message)) {
-                                    print ' has-error';
-                                }
-                                ?>"
+                            <input type='email' name='email' id='email'
+                                    placeholder='e.g., name@college.mak.ac.ug' class='fluid
+                                    <?php
+                                    if (isset($error_message)) {
+                                        print ' has-error';
+                                    }
+                                    ?>'
 
                                 <?php
                                 if (isset($email)) {
@@ -41,7 +41,7 @@
                             ?>
                         </div>
                     </fieldset>
-                    <input type="submit" value="Next &rarr;" class="btn btn-sm">
+                    <input type='submit' value='Next &rarr;' class='btn btn-sm'>
                 </form>
             <?php } ?>
         </div><!-- box -->
--- application/views/register/step-three.php
+++ application/views/register/step-three.php
@@ -1,24 +1,24 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
-<div class="wrap-single">
-    <div role="main" class="main">
-        <div class="box">
+<div class='wrap-single'>
+    <div role='main' class='main'>
+        <div class='box'>
             <h4>Sign Up: step 3 of 3</h4>
-            <form action="<?= base_url('register/step-three'); ?>" method="post"
-                accept-charset="utf-8" role="form">
+            <form action='<?= base_url('register/step-three'); ?>' method='post'
+                    accept-charset='utf-8' role='form'>
                 <fieldset>
-                    <div class="form-group">
-                        <label for="user-name">Username:</label>
-                        <span class="help-block">
+                    <div class='form-group'>
+                        <label for='user-name'>Username:</label>
+                        <span class='help-block'>
                             Must be atleast 3 characters long<br>
                             Only letters and numbers are allowed.
                         </span>
-                        <input type="text" name="uname" id="user-name" class="fluid
+                        <input type='text' name='uname' id='user-name' class='fluid
                         <?php
                         if (isset($error_messages) && isset($error_messages['uname'])) {
                             print ' has-error';
                         }
-                        ?>"
+                        ?>'
                         <?php
                         if (isset($uname)) {
                             print " value='{$uname}'";
@@ -30,32 +30,32 @@
                         }
                         ?>
                     </div>
-                    <div class="form-group">
-                        <label for="passwd1">Password:</label>
-                        <span class="help-block">
+                    <div class='form-group'>
+                        <label for='passwd1'>Password:</label>
+                        <span class='help-block'>
                             Must be atleast 6 characters long.<br>
                             Include atleast one uppercase letter, a lowercase letter, and a number.
                         </span>
-                        <input type="password" name="passwd1" id="passwd1" class="fluid
+                        <input type='password' name='passwd1' id='passwd1' class='fluid
                         <?php
                         if (isset($error_messages) && isset($error_messages['passwd1'])) {
                             print ' has-error';
                         }
-                        ?>" required>
+                        ?>' required>
                         <?php
                         if (isset($error_messages) && isset($error_messages['passwd1'])) {
                             print "<span class='error'>{$error_messages['passwd1']}</span>";
                         }
                         ?>
                     </div>
-                    <div class="form-group">
-                        <label for="passwd2">Confirm Password:</label>
-                        <input type="password" name="passwd2" id="passwd2" class="fluid
+                    <div class='form-group'>
+                        <label for='passwd2'>Confirm Password:</label>
+                        <input type='password' name='passwd2' id='passwd2' class='fluid
                         <?php
                         if (isset($error_messages) && isset($error_messages['passwd2'])) {
                             print ' has-error';
                         }
-                        ?>" required>
+                        ?>' required>
                         <?php
                         if (isset($error_messages) && isset($error_messages['passwd2'])) {
                             print "<span class='error'>{$error_messages['passwd2']}</span>";
@@ -63,6 +63,6 @@
                         ?>
                     </div>
                 </fieldset>
-                <input type="submit" name="submit" value="Sign Up" class="btn btn-sm">
+                <input type='submit' name='submit' value='Sign Up' class='btn btn-sm'>
             </form>
         </div><!-- box -->
