913b9cb23a36caab5782325bf73d7282e5dc5ddf
Lil Foo <robertelvisodoch@gmail.com>
2017-07-23 15:08:13 +0300

    Controller and model for "link"

--- application/controllers/Link.php
+++ application/controllers/Link.php
@@ -13,7 +13,7 @@ class Link extends CI_Controller
             redirect(base_url('login'));
         }
 
-        $this->load->model(['user_model', 'utility_model']);
+        $this->load->model(['user_model', 'link_model', 'utility_model']);
 
         // Check whether the user hasn't been logged out from some where else.
         $this->user_model->confirm_logged_in();
@@ -25,8 +25,338 @@ class Link extends CI_Controller
         $data['title'] = 'Add link to a resource from another website';
         $this->load->view('common/header', $data);
 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $link_url = $this->input->post('link_url');
+            if (!filter_var($link_url, FILTER_VALIDATE_URL) ||
+                !preg_match('/^(http[s]?:\/\/)/', $link_url)) {
+                $data['link_url'] = $link_url;
+                $data['error_message'] = 'Please enter a valid URL.';
+            }
+            else {
+                $link_data = $this->get_link_data($link_url);
+                $this->link_model->publish($link_data);
+                redirect(base_url("user/{$_SESSION['user_id']}"));
+            }
+        }
+
         $this->load->view('add-link', $data);
         $this->load->view('common/footer');
+    }
+
+    public function add_comment($link_id = 0)
+    {
+        try {
+            $link = $this->link_model->get_link($link_id);
+        }
+        catch (NotFoundException $e) {
+            show_404();
+        }
+
+        if ($link['user_id'] != $_SESSION['user_id']) {
+            $this->utility_model->show_error(
+                'Permission Denied!',
+                'You don\'t have the proper permissions.'
+            );
+            return;
+        }
+
+        $data = $this->user_model->initialize_user();
+        $data['title'] = 'Say something about this link';
+        $this->load->view('common/header', $data);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $comment = trim(strip_tags($this->input->post('description')));
+            if (strlen($comment) == 0) {
+                $data['error_message'] = 'Please enter a comment.';
+            }
+            else {
+                $this->link_model->add_comment($comment, $link_id);
+                redirect(base_url("user/{$_SESSION['user_id']}"));
+            }
+        }
+
+        $data['link'] = $link;
+        $data['item'] = 'link';
+        $data['form_action'] = base_url("link/add-comment/{$link_id}");
+        $this->load->view('add-description', $data);
+        $this->load->view('common/footer');
+    }
+
+    public function like($link_id = 0)
+    {
+        try {
+            $this->link_model->like($link_id);
+            redirect($_SERVER['HTTP_REFERER']);
+        }
+        catch (NotFoundException $e) {
+            show_404();
+        }
+        catch (IllegalAccessException $e) {
+            $this->utility_model->show_error(
+                'Permission Denied!',
+                'You don\'t have the proper permissions to like this link.'
+            );
+        }
+    }
+
+    public function comment($link_id = 0)
+    {
+        try {
+            $link = $this->link_model->get_link($link_id);
+        }
+        catch (NotFoundException $e) {
+            show_404();
+        }
+
+        if (!$this->user_model->are_friends($link['user_id'])) {
+            $this->utility_model->show_error(
+                'Permission Denied!',
+                'You don\'t have the proper permissions to comment on this link.'
+            );
+            return;
+        }
+
+        $data = $this->user_model->initialize_user();
+        $data['title'] = 'Comment on this link';
+        $this->load->view('common/header', $data);
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $comment = trim(strip_tags($this->input->post('comment')));
+            if (!$comment) {
+                $data['comment_error'] = 'Please enter a comment.';
+            }
+            else {
+                $this->link_model->comment($link_id, $comment);
+                $this->comments($link_id);
+                return;
+            }
+        }
+
+        $data['link'] = $link;
+        $data['object'] = 'link';
+        $this->load->view('comment', $data);
+        $this->load->view('common/footer');
+    }
+
+    public function share($link_id = 0)
+    {
+        try {
+            $this->link_model->share($link_id);
+            redirect(base_url("user/{$_SESSION['user_id']}"));
+        }
+        catch (NotFoundException $e) {
+            show_404();
+        }
+        catch (IllegalAccessException $e) {
+            $this->utility_model->show_error(
+                'Permission Denied!',
+                'You don\'t have the proper permissions to share this link.'
+            );
+        }
+    }
+
+    public function likes($link_id = 0, $offset = 0)
+    {
+        try {
+            $link = $this->link_model->get_link($link_id);
+        }
+        catch (NotFoundException $e) {
+            show_404();
+        }
+
+        $data = $this->user_model->initialize_user();
+        $data['title'] = 'People who liked this link';
+        $this->load->view('common/header', $data);
+
+        // Maximum number of likes to display.
+        $limit = 10;
+
+        if ($offset != 0) {
+            $data['has_prev'] = TRUE;
+            $data['prev_offset'] = 0;
+            if ($offset > $limit) {
+                $data['prev_offset'] = ($offset - $limit);
+            }
+        }
+
+        $data['has_next'] = FALSE;
+        if (($link['num_likes'] - $offset) > $limit) {
+            $data['has_next'] = TRUE;
+            $data['next_offset'] = ($offset + $limit);
+        }
+
+        $data['num_prev'] = $offset;
+        $data['likes'] = $this->link_model->get_likes($link, $offset, $limit);
+
+        $data['object'] = 'link';
+        $data['link'] = $link;
+        $this->load->view('show/likes', $data);
+        $this->load->view('common/footer');
+    }
+
+    public function comments($link_id = 0, $offset = 0)
+    {
+        try {
+            $link = $this->link_model->get_link($link_id);
+        }
+        catch (NotFoundException $e) {
+            show_404();
+        }
+
+        $data = $this->user_model->initialize_user();
+        $data['title'] = 'Comments on this link';
+        $this->load->view('common/header', $data);
+
+        // Maximum number of comments to display.
+        $limit = 10;
+
+        if ($offset != 0) {
+            $data['has_prev'] = TRUE;
+            $data['prev_offset'] = 0;
+            if ($offset > $limit) {
+                $data['prev_offset'] = ($offset - $limit);
+            }
+        }
+
+        $data['has_next'] = FALSE;
+        if (($link['num_comments'] - $offset) > $limit) {
+            $data['has_next'] = TRUE;
+            $data['next_offset'] = ($offset + $limit);
+        }
+
+        $data['comments'] = $this->link_model->get_comments($link, $offset, $limit);
+        $data['object'] = 'link';
+        $data['link'] = $link;
+        $this->load->view('show/comments', $data);
+        $this->load->view('common/footer');
+    }
+
+    public function shares($link_id = 0, $offset = 0)
+    {
+        try {
+            $link = $this->link_model->get_link($link_id);
+        }
+        catch (NotFoundException $e) {
+            show_404();
+        }
+
+        $data = $this->user_model->initialize_user();
+        $data['title'] = 'People who shared this link';
+        $this->load->view('common/header', $data);
+
+        // Maximum number of shares to display.
+        $limit = 10;
+
+        if ($offset != 0) {
+            $data['has_prev'] = TRUE;
+            $data['prev_offset'] = 0;
+            if ($offset > $limit) {
+                $data['prev_offset'] = ($offset - $limit);
+            }
+        }
+
+        $data['has_next'] = FALSE;
+        if (($link['num_shares'] - $offset) > $limit) {
+            $data['has_next'] = TRUE;
+            $data['next_offset'] = ($offset + $limit);
+        }
+
+        $data['num_prev'] = $offset;
+        $data['shares'] = $this->link_model->get_shares($link, $offset, $limit);
+
+        $data['object'] = 'link';
+        $data['link'] = $link;
+        $this->load->view('show/shares', $data);
+        $this->load->view('common/footer');
+    }
+
+    private function get_link_data($url)
+    {
+        $data = [
+            'url' => $url,
+            'title' => '',
+            'description' => '',
+            'image' => '',
+            'site' => parse_url($url, PHP_URL_HOST)
+        ];
+
+        /// Fetch Open Graph metas.
+        $og_metas = $this->fetch_og($url);
+
+        if (!empty($og_metas['title'])) {
+            $data['title'] = $og_metas['title'];
+        }
+        if (!empty($og_metas['description'])) {
+            $data['description'] = $og_metas['description'];
+        }
+        if (!empty($og_metas['image'])) {
+            $data['image'] = $og_metas['image'];
+        }
+
+        /// If OG metas can't be found, try twitter metas.
+
+        // Fetch named meta tags.
+        $meta_tags = get_meta_tags($url);
+
+        if (empty($data['title']) && empty($data['description'] && empty($data['image']))) {
+            if (!empty($meta_tags['twitter:title'])) {
+                $data['title'] = $meta_tags['twitter:title'];
+            }
+            if (!empty($meta_tags['twitter:description'])) {
+                $data['description'] = $meta_tags['twitter:description'];
+            }
+            if (!empty($meta_tags['twitter:image'])) {
+                $data['image'] = $meta_tags['twitter:image'];
+            }
+        }
+
+        /// If data is still empty, then fall back to standard HTML title and description.
+        if (empty($data['title']) && empty($data['description'])) {
+            $html_data = file_get_contents($url);
+            $dom = new DomDocument;
+            @$dom->loadHTML($html_data);
+
+            $xpath = new DOMXPath($dom);
+            # query metatags with og prefix
+            $metas = $xpath->query('//*/title');
+            $title = $metas[0]->textContent;
+            $data['title'] = $title;
+
+            if (!empty($meta_tags['description'])) {
+                $data['description'] = $meta_tags['description'];
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * Fetch OG Metatags
+     * @param string $url
+     *
+     * @return array
+     */
+    private function fetch_og($url)
+    {
+        $data = file_get_contents($url);
+        $dom = new DomDocument;
+        @$dom->loadHTML($data);
+
+        $xpath = new DOMXPath($dom);
+        # query metatags with og prefix
+        $metas = $xpath->query('//*/meta[starts-with(@property, \'og:\')]');
+
+        $og = array();
+
+        foreach($metas as $meta){
+            # get property name without og: prefix
+            $property = str_replace('og:', '', $meta->getAttribute('property'));
+            # get content
+            $content = $meta->getAttribute('content');
+            $og[$property] = $content;
+        }
+
+        return $og;
     }
 }
 ?>
---
+++ application/models/Link_model.php
@@ -0,0 +1,227 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once('classes/SimpleLink.php');
+require_once('exceptions/IllegalAccessException.php');
+require_once('exceptions/NotFoundException.php');
+
+/**
+ * Contains functions related to a link.
+ */
+class Link_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model([
+            'utility_model', 'activity_model',
+            'user_model', 'comment_model'
+        ]);
+    }
+
+    /**
+     * Gets a link plus other link metadata.
+     *
+     * Throws NotFoundException if the link cannot be found.
+     *
+     * @param $link_id the ID of the link in the links table.
+     * @return the link with the given ID.
+     */
+    public function get_link($link_id)
+    {
+        $link_sql = sprintf("SELECT v.*, u.profile_name AS author FROM links v " .
+                            "LEFT JOIN users u ON(v.user_id = u.user_id) " .
+                            "WHERE link_id = %d",
+                            $link_id);
+        $link_query = $this->utility_model->run_query($link_sql);
+        if ($link_query->num_rows() == 0) {
+            throw new NotFoundException();
+        }
+        $link = $link_query->row_array();
+        $link['profile_pic_path'] = $this->user_model->get_profile_pic_path($link['user_id']);
+        $link['timespan'] = timespan(mysql_to_unix($link['date_entered']), now(), 1);
+
+        // Add data used by views.
+        $link['has_comment'] = strlen($link['comment']) != 0;
+        $link['shared'] = FALSE;
+
+        // Check whether the user currently viewing the page is a friend to the
+        // owner of the link. This will allow us to only show the like, comment
+        // and share buttons to friends of the owner.
+        $link['viewer_is_friend_to_owner'] = $this->user_model->are_friends($link['user_id']);
+
+        // Get number of liks, comments and shares.
+        $simpleLink = new SimpleLink($link['link_id'], 'link', $link['user_id']);
+        $link['num_likes'] = $this->activity_model->getNumLikes($simpleLink);
+        $link['num_comments'] = $this->activity_model->getNumComments($simpleLink);
+        $link['num_shares'] = $this->activity_model->getNumShares($simpleLink);
+
+        return $link;
+    }
+
+    /**
+     * Allows a user to add a new link to his status.
+     *
+     * @param $url.
+     * @return $link_id the ID of the link in the user_links table.
+     */
+    public function publish($link_data)
+    {
+        // Record link data in the links table.
+        $link_sql = sprintf('INSERT INTO links (user_id, url, title, description, image, site) ' .
+                            'VALUES (%d, %s, %s, %s, %s, %s) ',
+                            $_SESSION['user_id'], $this->db->escape($data['url']),
+                            $this->db->escape($data['title']), $this->db->escape($data['description']),
+                            $this->db->escape($data['image']), $this->db->escape($data['site']));
+        $this->utility_model->run_query($link_sql);
+        $link_id = $this->db->insert_id();
+
+        // Dispatch an activity.
+        $activity_sql = sprintf('INSERT INTO activities ' .
+                                '(actor_id, subject_id, source_id, source_type, activity) ' .
+                                'VALUES (%d, %d, %d, "link", "link")',
+                                $_SESSION['user_id'], $_SESSION['user_id'], $link_id);
+        $this->utility_model->run_query($activity_sql);
+    }
+
+    /**
+     * Adds a brief description about a link.
+     *
+     * Assumes that the link alreay exists in the user_links table.
+     *
+     * @param $description the description entered by the user.
+     * @param $link_id the id of the link in the user_links table.
+     */
+    public function add_comment($comment, $link_id)
+    {
+        $sql = sprintf('UPDATE links SET comment = %s WHERE (link_id = %d)',
+                        $this->db->escape($comment), $link_id);
+        $this->utility_model->run_query($sql);
+    }
+
+    /**
+     * Records a like of a link.
+     *
+     * Throws NotFoundException exception if link is not on record.
+     * It may also throw IllegalAccessException if a user attempts to like
+     * a link published by a user who is not his friend.
+     *
+     * @param $link_id the ID of the link in the user_links table.
+     */
+    public function like($link_id)
+    {
+        // Get the id of the owner of this link.
+        $owner_sql = sprintf('SELECT user_id FROM links WHERE link_id = %d', $link_id);
+        $owner_query = $this->utility_model->run_query($owner_sql);
+        if ($owner_query->num_rows() == 0) {
+            throw new NotFoundException();
+        }
+
+        $owner_result = $owner_query->row_array();
+        $owner_id = $owner_result['user_id'];
+
+        if (!$this->user_model->are_friends($owner_id)) {
+            throw new IllegalAccessException();
+        }
+
+        $this->activity_model->like(new SimpleLink($link_id, 'link', $owner_id));
+    }
+
+    /**
+     * Shares a link on a user's timeline.
+     *
+     * Throws NotFoundException if a link is not on record.
+     * It may also throw IllegalAccessException if a user attempts to share
+     * a link that was published by a user who is not his friend.
+     *
+     * @param $link_id the ID of the link in the user_links table.
+     */
+    public function share($link_id)
+    {
+        $owner_sql = sprintf('SELECT user_id FROM links WHERE link_id = %d', $link_id);
+        $owner_query = $this->utility_model->run_query($owner_sql);
+        if ($owner_query->num_rows() == 0) {
+            throw new NotFoundException();
+        }
+
+        $owner_result = $owner_query->row_array();
+        $owner_id = $owner_result['user_id'];
+        if (!$this->user_model->are_friends($owner_id)) {
+            throw new IllegalAccessException();
+        }
+
+        $this->activity_model->share(new SimpleLink($link_id, 'link', $owner_id));
+    }
+
+    /**
+     * Records a comment on a link.
+     *
+     * @param $link_id the ID of the link in the user_links table.
+     * @param $comment the comment a user made.
+     */
+    public function comment($link_id, $comment)
+    {
+        // Get the ID of the owner of this link.
+        $owner_sql = sprintf('SELECT user_id FROM links WHERE link_id = %d', $link_id);
+        $owner_result = $this->utility_model->run_query($owner_sql)->row_array();
+        $owner_id = $owner_result['user_id'];
+
+        // Record the comment.
+        $this->activity_model->comment(
+            new SimpleLink($link_id, 'link', $owner_id),
+            $comment
+        );
+    }
+
+    /**
+     * Get users who liked a link.
+     *
+     * @param $link an array containing link data.
+     * @param offset the position to begin returning records from.
+     * @param $limit the maximum number of records to return.
+     * @return the users who liked this link.
+     */
+    public function get_likes(&$link, $offset, $limit)
+    {
+        return $this->activity_model->getLikes(
+            new SimpleLink($link['link_id'], 'link', $link['user_id']),
+            $offset,
+            $limit
+        );
+    }
+
+    /**
+     * Gets the users who shared a link.
+     *
+     * @param $link an array containing link data.
+     * @param offset the position to begin returning records from.
+     * @param $limit the maximum number of records to return.
+     * @return the users who shared this link.
+     */
+    public function get_shares(&$link, $offset, $limit)
+    {
+        return $this->activity_model->getShares(
+            new SimpleLink($link['link_id'], 'link', $link['user_id']),
+            $offset,
+            $limit
+        );
+    }
+
+    /**
+     * Gets the comments made on a link.
+     *
+     * @param $link an array containing link data.
+     * @param offset the position to begin returning records from.
+     * @param $limit the maximum number of records to return.
+     * @return the comments made on this link.
+     */
+    public function get_comments(&$link, $offset, $limit)
+    {
+        return $this->activity_model->getComments(
+            new SimpleLink($link['link_id'], 'link', $link['user_id']),
+            $offset,
+            $limit
+        );
+    }
+}
+?>
