26ef12a9d2d80eea3b793dd738914db112176651
Lil Foo <robertelvisodoch@gmail.com>
2017-08-16 21:45:09 +0300

    Show a message when there are no photos

--- application/views/show/photos.php
+++ application/views/show/photos.php
@@ -5,10 +5,17 @@ require_once(dirname(__FILE__) . '/../common/user-page-start.php');
 require_once(dirname(__FILE__) . '/../common/secondary-user-nav.php');
 ?>
 
-<?php
-foreach ($photos as $photo) {
-    require(dirname(__FILE__) . '/../common/photo.php');
-}
+<?php if (count($photos) == 0): ?>
+    <div class='box'>
+        <div class='alert alert-info'>
+            <p>No photos to show.</p>
+        </div>
+    </div>
+<?php else:
+    foreach ($photos as $photo) {
+        require(dirname(__FILE__) . '/../common/photo.php');
+    }
+endif;
 ?>
 
 <?php if ($has_next) { ?>
