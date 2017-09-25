0a7376a06d67b8b102a23bdb4d16ecc17f32a8bb
Lil Foo <robertelvisodoch@gmail.com>
2017-09-25 17:03:22 +0300

    Improve interface for profile.

--- application/views/profile.php
+++ application/views/profile.php
@@ -5,152 +5,181 @@ require_once('common/user-page-start.php');
 require_once("common/secondary-user-nav.php");
 ?>
 
-<div class='box show-profile'>
-    <?php
-    if ( ! empty($profile_questions)) {
+<?php
+if ( ! empty($profile_questions)) {
+    print "div class='box'>";
         foreach ($profile_questions as $pq) {
             print $pq;
         }
-    }
+    print "</div>";
+}
 
-    // If there is nothing to show.
-    if ($is_visitor && !$profile['schools'] && !$profile['halls']
-        && !$profile['hostels'] && !$profile['origin']['district']) {
-        print "<div class='alert alert-info' role='alert'>
-                <span class='glyphicon glyphicon-info-sign' aria-hidden='true'></span>
-                <p>Nothing to show.</p></div>";
-    }
-    else {
-    ?>
-        <?php if (!$is_visitor || $profile['schools']) { ?>
-            <h4>Education</h4>
-            <ul class='profile'>
-            <?php
-            if ($profile['schools']) {
-                foreach ($profile['schools'] as $sch) {
-                    print "<span><b>{$sch['start_year']} - {$sch['end_year']}</b></span>";
-                    if ($sch['college_name']) {
-                        print "<li><b>College: </b>{$sch['college_name']}</li>";
-                    }
+// If there is nothing to show.
+if ($is_visitor && !$profile['schools'] && !$profile['halls']
+    && !$profile['hostels'] && !$profile['origin']['district']) {
+    print "<div class='box'>
+                <div class='alert alert-info' role='alert'>
+                    <span class='glyphicon glyphicon-info-sign' aria-hidden='true'></span>
+                    <p>Nothing to show.</p>
+                </div>
+            </div>";
+}
+?>
 
-                    print "<li><b>School: </b>{$sch['school_name']}";
+<?php if (!$is_visitor || $profile['schools']) { ?>
+    <div class='box profile'>
+        <h4>Education</h4>
+        <?php
+        if ($profile['schools']) {
+            foreach ($profile['schools'] as $sch) {
+                print "<p class='year'><span>{$sch['start_year']} - {$sch['end_year']}</span></p>";
+
+                print "<table class='table table-bordered'>";
+
+                if ($sch['college_name']) {
+                    print "<tr><td><b>College</b></td><td>{$sch['college_name']}</td></tr>";
+                }
+
+                print "<tr><td><b>School</b></td><td>{$sch['school_name']}";
+                if (!$is_visitor) {
+                    print " <a href='" . base_url("profile/edit-school/{$sch['id']}") .
+                        "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> Edit</a>";
+                }
+                print "</td></tr>";
+
+                if ($sch['has_programme']) {
+                    print "<tr><td><b>Programme</b></td><td>{$sch['programme']['programme_name']}";
                     if (!$is_visitor) {
-                        print " <a href='" . base_url("profile/edit-school/{$sch['id']}") .
+                        print " <a href='" . base_url("profile/edit-programme/{$sch['programme']['id']}") .
                             "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> Edit</a>";
                     }
-                    print "</li>";
-
-                    if ($sch['has_programme']) {
-                        print "<li><b>Programme: </b>{$sch['programme']['programme_name']}";
-                        if (!$is_visitor) {
-                            print " <a href='" . base_url("profile/edit-programme/{$sch['programme']['id']}") .
-                                "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> Edit</a>";
-                        }
-                        print "</li>";
-                    }
-                    else {
-                        if (!$is_visitor) {
-                            print "<li><b>Programme: </b> <a href='".
-                                    base_url("profile/add-programme/{$sch['id']}") .
-                                    "'>Add programme</a></li>";
-                        }
-                    }
-                }  // End foreach.
-
-                if (!$is_visitor) {
-                    print "<a href='". base_url("profile/add-school") . "'>Add school</a>";
-                }
-            }
-            elseif (!$is_visitor) {
-                print "<a href='". base_url("profile/add-school") . "'>Add school</a>";
-            } // ($profile['colleges']).
-
-            print "</ul>";
-        } // (!$is_visitor || $profile['colleges']) ?>
-
-        <?php if (!$is_visitor || ($profile['hostels'] || $profile['halls'])) { ?>
-            <h4>Residence</h4>
-            <ul class='profile'>
-                <?php
-                if ($profile['halls']) {
-                    foreach ($profile['halls'] as $hall) {
-                        if ($hall['resident']) {
-                            print "<li><b>{$hall['start_year']} - {$hall['end_year']}: </b>" .
-                                    "Resident of {$hall['hall_name']}";
-                            if (!$is_visitor) {
-                                print " <a href='" . base_url("profile/edit-hall/{$hall['id']}") .
-                                        "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> Edit</a>";
-                            }
-                            print "</li>";
-                        }
-                        else {
-                            print "<li><b>{$hall['start_year']} - {$hall['end_year']}: </b>" .
-                                    "Attached to {$hall['hall_name']}";
-                            if (!$is_visitor) {
-                                print " <a href='" . base_url("profile/edit-hall/{$hall['id']}") .
-                                        "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> Edit</a>";
-                            }
-                            print "</li>";
-                        }
-                    }
-
-                    if (!$is_visitor) {
-                        print "<a href='" . base_url("profile/add-hall") . "'>Add hall</a>";
-                    }
+                    print "</td></tr>";
                 }
                 else {
-                    print "<a href='" . base_url("profile/add-hall") . "'>Add hall</a>";
-                }
-
-                if ($profile['hostels']) {
-                    foreach ($profile['hostels'] as $hostel) {
-                        print "<li><b>{$hostel['start_year']} - {$hostel['end_year']}: </b>" .
-                                "{$hostel['hostel_name']}";
-                        if (!$is_visitor) {
-                            print " <a href='" . base_url("profile/edit-hostel/{$hostel['id']}") .
-                                    "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> Edit</a>";
-                        }
-                        print "</li>";
+                    if (!$is_visitor) {
+                        print "<tr><td><b>Programme</b></td><td><a href='".
+                                base_url("profile/add-programme/{$sch['id']}") .
+                                "'>Add programme</a></td></tr>";
                     }
                 }
-                if (!$is_visitor) {
-                    if (!$profile['hostels']) {
-                        print ' &middot; ';
-                    }
-                    print "<a href='" . base_url("profile/add-hostel") . "'>Add hostel</a>";
-                }
-                ?>
-            </ul>
-        <?php } // if ($profile['halls'] || $profile['hostels']) ?>
 
-        <?php if (!$is_visitor || $profile['origin']['district']) { ?>
-            <h4>Origin</h4>
-            <ul class='profile'>
-                <?php
-                if (!$is_visitor) {
-                    if ($profile['origin']['district']) {
-                        print "<li><b>District:</b> {$profile['origin']['district']}
-                                <a href='" . base_url("profile/edit-district") .
-                                "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> Edit</a>
-                                </li>";
+                print "</table>";
+            }  // End foreach.
 
-                        print "<li><b>Country:</b> {$profile['origin']['country']}</li>";
-                    }
-                    else {
-                        print "<li><b>District:</b> <a href='" . base_url("profile/add-district") .
-                                "'>Add district</a></li>";
-                    }
-                } else {
-                ?>
-                    <li>
+            if (!$is_visitor) {
+                print "<a href='". base_url("profile/add-school") . "' class='pull-right'>Add school</a>
+                        <span class='clearfix'></span>";
+            }
+        }
+        else {
+            if (!$is_visitor) {
+                print "<a href='". base_url("profile/add-school") . "'>Add school</a>";
+            }
+        }  // ($profile['schools'])
+        ?>
+    </div>
+
+<?php } // (!$is_visitor || $profile['schools']) ?>
+
+<?php if (!$is_visitor || ($profile['hostels'] || $profile['halls'])) { ?>
+    <div class='box profile'>
+        <h4>Residence</h4>
+        <?php if ($profile['halls'] || $profile['hostels']): ?>
+            <table class='table table-bordered'>
+                <thead>
+                    <tr>
+                        <th>Year</th>
+                        <th>Hall/Hostel</th>
+                    </tr>
+                </thead>
+                <tbody>
                     <?php
-                    if ($profile['origin']['district']) {
-                        print "From {$profile['origin']['district']}, {$profile['origin']['country']}.";
+                    if ($profile['halls']) {
+                        foreach ($profile['halls'] as $hall) {
+                            print "<tr><td>{$hall['start_year']} - {$hall['end_year']}</td>";
+                            if ($hall['resident']) {
+                                print "<td>Resident of {$hall['hall_name']}";
+                                if (!$is_visitor) {
+                                    print " <a href='" . base_url("profile/edit-hall/{$hall['id']}") .
+                                            "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> Edit</a>";
+                                }
+                                print "</td>";
+                            }
+                            else {
+                                print "<td>Attached to {$hall['hall_name']}";
+                                if (!$is_visitor) {
+                                    print " <a href='" . base_url("profile/edit-hall/{$hall['id']}") .
+                                            "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> Edit</a>";
+                                }
+                                print "</td>";
+                            }
+                            print "</tr>";
+                        }
+                    }
+
+                    if ($profile['hostels']) {
+                        foreach ($profile['hostels'] as $hostel) {
+                            print "<tr><td>{$hostel['start_year']} - {$hostel['end_year']}</td>";
+
+                            print "<td>{$hostel['hostel_name']}";
+                            if (!$is_visitor) {
+                                print " <a href='" . base_url("profile/edit-hostel/{$hostel['id']}") .
+                                        "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> Edit</a>";
+                            }
+                            print "</td></tr>";
+                        }
                     }
                     ?>
-                    </li>
-                <?php } // (!$is_visitor) ?>
-            </ul>
-        <?php } // End (!$is_visitor \\ ($profile['country'] || $profile['district'])) ?>
-    <?php } // End nothing to show.?>
-</div><!-- box -->
+                </tdoby>
+            </table>
+
+            <?php
+            if (!$is_visitor) {
+                print "<a href='" . base_url("profile/add-hall") . "' class='pull-right'>Add hall</a>
+                        <span class='pull-right'>&nbsp;&nbsp;&middot;&nbsp;&nbsp;</span>
+                        <a href='" . base_url("profile/add-hostel") . "' class='pull-right'>Add hostel</a>
+                        <span class='clearfix'></span>";
+            }
+            ?>
+        <?php else:
+            if (!$is_visitor) {
+                print "<a href='" . base_url("profile/add-hall") . "'>Add hall</a>
+                        <span>&nbsp;&nbsp;&middot;&nbsp;&nbsp;</span>
+                        <a href='" . base_url("profile/add-hostel") . "'>Add hostel</a>";
+            }
+        endif;?>
+    </div>
+<?php } // if ($profile['halls'] || $profile['hostels']) ?>
+
+<?php if (!$is_visitor || $profile['origin']['district']) { ?>
+    <div class='box profile'>
+        <h4>Origin</h4>
+        <?php
+        if (!$is_visitor) {
+            print "<table class='table table-bordered'>";
+
+            if ($profile['origin']['district']) {
+                print "<tr><td><b>District</b></td><td>{$profile['origin']['district']}
+                        <a href='" . base_url("profile/edit-district") .
+                        "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> Edit</a>
+                        </td></tr>";
+
+                print "<tr><td><b>Country</b></td><td>{$profile['origin']['country']}</td></tr>";
+            }
+            else {
+                print "<a href='" . base_url("profile/add-district") . "'>Add district</a>";
+            }
+
+            print "</table>";
+        } else {
+        ?>
+            <p>
+                <?php
+                if ($profile['origin']['district']) {
+                    print "From {$profile['origin']['district']}, {$profile['origin']['country']}.";
+                }
+                ?>
+            </p>
+        <?php } // (!$is_visitor) ?>
+    </div>
+<?php } // End (!$is_visitor \\ ($profile['country'] || $profile['district'])) ?>
