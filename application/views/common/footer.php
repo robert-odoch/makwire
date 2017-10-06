c281414390f02616d320756a938ecbd8ffcdb5c8
Robert Odoch <robertelvisodoch@gmail.com>
2017-10-06 15:17:22 +0300

    Change site footer.

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -15,20 +15,20 @@
         </div> <!-- #wrapper-* -->
 
         <footer role='contentinfo' class='footer site-footer'>
-            <p id='site-copyright'>Copyright &copy; <?php echo date('Y'); ?> makwire</p>
+            <p id='site-copyright'>Makwire &copy; <?= date('Y'); ?></p>
         </footer>
 
         <!-- Need JQuery to support Bootstrap JQuery plugins -->
         <script src='<?= base_url('scripts/jquery-1.11.3.js'); ?>'></script>
 
         <!-- Bootsrap core JavaScript -->
-        <script src='<?php echo base_url('scripts/bootstrap.min.js'); ?>'></script>
+        <script src='<?= base_url('scripts/bootstrap.min.js'); ?>'></script>
 
         <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-        <script src='<?php echo base_url('scripts/ie10-viewport-bug-workaround.js'); ?>'></script>
+        <script src='<?= base_url('scripts/ie10-viewport-bug-workaround.js'); ?>'></script>
 
         <!-- And then our very own. -->
-        <script src='<?php echo base_url('scripts/plugins/jquery.scrollTo-2.1.2/jquery.scrollTo.min.js'); ?>'></script>
-        <script src='<?php echo base_url('scripts/site.js'); ?>'></script>
+        <script src='<?= base_url('scripts/plugins/jquery.scrollTo-2.1.2/jquery.scrollTo.min.js'); ?>'></script>
+        <script src='<?= base_url('scripts/site.js'); ?>'></script>
     </body>
 </html>
