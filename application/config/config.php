a5dadaa73f08876474223327806ec72324ef167b
Robert Odoch <robertelvisodoch@gmail.com>
2017-10-07 09:34:22 +0300

    Set default timezone.

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/* Set default timezone. */
+date_default_timezone_set('Africa/Kampala');
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
