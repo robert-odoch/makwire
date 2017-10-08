59908781b247b546d361baddb3c8b07f4291c2ca
Robert Odoch <robertelvisodoch@gmail.com>
2017-10-08 22:23:12 +0300

    Remove secondary user nav from change profile picture.

--- application/views/add/profile-picture.php
+++ application/views/add/profile-picture.php
@@ -1,7 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 require_once(__DIR__ . '/../common/user-page-start.php');
-require_once(__DIR__ . '/../common/secondary-user-nav.php');
 ?>
 
 <div class='box'>
