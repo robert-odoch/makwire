60c8f5c57bf0ab86deb76cb0a1db4c1f25e0e316
Robert Odoch <robertelvisodoch@gmail.com>
2017-10-11 01:43:48 +0300

    Make the width of email 80%

--- application/views/email.php
+++ application/views/email.php
@@ -1,5 +1,5 @@
  <div style='margin: 0; background-color: #fff;'>
-    <div style='width: 50%; margin: auto; margin-top: 15px; border: 1px solid green;'>
+    <div style='width: 80%; margin: auto; margin-top: 15px; border: 1px solid green;'>
       <div style='color: #fff; padding: 10px 5px; text-align: center; background-color: green;'>
         <h1 style='margin: 0; text-transform: lowercase; font-variant: small-caps;'>
             <?php echo $email_heading; ?>
