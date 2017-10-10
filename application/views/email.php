7a0b552dc6ac119ba695332cc34318891a62c733
Robert Odoch <robertelvisodoch@gmail.com>
2017-10-11 01:44:54 +0300

    Make copyright notice consistent with site copyright notice.

--- application/views/email.php
+++ application/views/email.php
@@ -9,7 +9,7 @@
         <?php echo $message; ?>
       </div>
       <div style='color: green; padding: 10px 5px; text-align: center; border-top: 1px solid green;'>
-        Makwire &copy; <?= date('Y'); ?>
+        &copy; <?= date('Y'); ?> Makwire
       </div>
     </div>
 </div>
