25f2178c104b9344251447af69b3e1475d70cb10
Robert Odoch <robertelvisodoch@gmail.com>
2017-10-10 10:49:51 +0300

    Remove timezone.

--- application/config/config.php
+++ application/config/config.php
@@ -1,9 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/* Set default timezone. */
-date_default_timezone_set('Africa/Kampala');
-
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
